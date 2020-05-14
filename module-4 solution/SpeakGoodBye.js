56fe2766707164a343cad89a1c7bbb39968c4e73
Mr-Vivekk <64269771+Mr-Vivekk@users.noreply.github.com>
2020-05-14 18:13:17 +0530

    Add files via upload

---
+++ module-4 solution/SpeakGoodBye.js
@@ -0,0 +1,7 @@
+(function (window) {var speakWord = "Good Bye";
+var byeSpeaker = {}; 
+ byeSpeaker.speak = function (name) {
+ console.log(speakWord + " " + name);
+}
+ window.byeSpeaker = byeSpeaker;
+})(window);
